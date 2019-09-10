a5e61cf94cfd0effc7c8487d1af14d970373b3e9
ALDA <pppppaaattt@gmail.com>
2019-09-11 01:58:20 +0800

    BINAGO

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing git
+          binago
         </p>
         <a
           className="App-link"
