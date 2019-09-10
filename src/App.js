e1608bcecc9d78bf575815e0eea2082ba24774d6
ALDA <pppppaaattt@gmail.com>
2019-09-11 02:16:04 +0800

    push

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          binago
+          ETO TEST
         </p>
         <a
           className="App-link"
