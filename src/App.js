a5dfac7aa7eff735eeda19060aea115a4ffb788d
ALDA <pppppaaattt@gmail.com>
2019-09-11 02:42:21 +0800

    tapos na

--- src/App.js
+++ src/App.js
@@ -4,12 +4,47 @@ import './App.css';
 
 class App extends React.Component {
 
+
+  state = {
+    name:'',
+    compared: ''
+  }
+
+
+  handleChange = (e)=>{
+
+    this.setState({name:e.target.value})
+    //console.log(this.state.name)
+  }
+
+  parehas = (e)=> {
+    this.setState({
+      compared:e.target.value
+    })
+  }
+  comparing=(e)=>{
+    if(this.state.name === this.state.compared){
+      alert("parehas")
+    }
+    else{
+      alert("diparehas")
+    }
+  }
+
   render(){
     return(
+    <form>
       <div>
-        <input type="text" />Enter Email
+        <input type="text" onChange={this.handleChange}/>Enter Email
       </div>
-
+      <div>
+        <input type="text" onChange={this.parehas}/>Confirm
+      </div>
+      <div>
+        <button onClick={this.comparing}>Compare</button>
+      </div>
+    </form>
+      
     )
 
 
