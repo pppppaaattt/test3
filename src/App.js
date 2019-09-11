623140bc11fc999bb0a2c3871addd0c8ec512dbe
jayzer05 <jhayzer.aranador05@gmail.com>
2019-09-11 10:43:57 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+   
+  state ={
+    passWord: ''
 
+  }
   render(){
     return(
       <div>
-        <input type="text" />Enter Email
+        <div>
+        <input id="pass" type="text" />Enter Password
+        </div>
+        <div>
+        <input type="text"  />Value
+        </div>
+        <button type="Submit">  Submit</button>
       </div>
 
     )
